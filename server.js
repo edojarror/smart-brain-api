e428b28d2fb4d32a6f94bbf1117b27b1334b7095
edojarror <edojarror@gmail.com>
2021-04-17 00:19:32 +0700

    try to fix CORS issue

--- server.js
+++ server.js
@@ -18,10 +18,17 @@ const db = knex ({
     }
   });
 
+const whitelist = ['https://fr-smart-brain.herokuapp.com']
 const corsOptions = {
-  origin: 'https://fr-smart-brain.herokuapp.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
+
 const app = express();
 
 app.use(cors());
