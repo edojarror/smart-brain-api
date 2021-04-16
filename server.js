2fd387668c56fb0c8f6099abcb6baa528827dae5
edojarror <edojarror@gmail.com>
2021-04-17 00:31:02 +0700

    add app.option to fix CORS issue

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.get('/', (req, res) => {
 
 
 app.post("/signin", (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
-
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+app.options('/register', cors())
+app.post('/register', cors(), (req, res, next) => {register.handleRegister(req, res, db, bcrypt)});
 
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 
