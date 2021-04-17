d9565cb9e78a0032221839fc52cf80d96648bf59
edojarror <edojarror@gmail.com>
2021-04-17 22:09:52 +0700

    solving CORS issue7

--- server.js
+++ server.js
@@ -25,6 +25,14 @@ const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
 };
 app.use(allowCrossDomain);
 
