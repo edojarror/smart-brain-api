f9126367b349dc8be4316baee7556f51e7a9f1f7
edojarror <edojarror@gmail.com>
2021-04-17 22:04:51 +0700

    solving CORS issue5

--- server.js
+++ server.js
@@ -23,16 +23,8 @@ const app = express();
 app.use(express.methodOverride());
 const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
 };
 app.use(allowCrossDomain);
 
