ed1c35ac01762336257336e17a488603e0514c26
edojarror <edojarror@gmail.com>
2021-04-17 22:50:59 +0700

    solving CORS issue15

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ const db = knex ({
   });
 
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next()
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next()
+// });
 
 app.use(bodyParser.json());
 // app.use(cors());
