793ebbfb9f9c047e0dae08cc3d97ad6fc10b7f3e
edojarror <edojarror@gmail.com>
2021-04-17 22:30:55 +0700

    solving CORS issue11

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("it is working!");
@@ -35,8 +35,8 @@ app.get('/', (req, res) => {
 
 
 app.post("/signin", (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
-app.options('/register', cors())
-app.post('/register',cors(), (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 
